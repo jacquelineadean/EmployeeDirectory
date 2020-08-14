f4097d0c0795f48f56ab973625dbdb62b97c23e4
Jacqueline Dean <jacquelineadean@gmail.com>
2020-08-14 18:44:30 -0500

    Updates

---
+++ precache-manifest.f110dbb7bcf8909d3d8499898905fb76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "752fe5c1c4a605d1d46d309dcdc1cc61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f23529d2c2d7c27d3680",
+    "url": "/static/css/main.63121b31.chunk.css"
+  },
+  {
+    "revision": "cfad87b817277ca2b5d5",
+    "url": "/static/js/2.4fe0c268.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4fe0c268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f23529d2c2d7c27d3680",
+    "url": "/static/js/main.efb3853c.chunk.js"
+  },
+  {
+    "revision": "35839d639db8e60032a1",
+    "url": "/static/js/runtime-main.f78ad96b.js"
+  }
+]);
