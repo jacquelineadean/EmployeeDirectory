a52b76f6de786ebeabeb805d551001d34fbc1dd5
Jacqueline Dean <jacquelineadean@gmail.com>
2020-08-14 21:11:27 -0500

    Took out router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 // Dependencies
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 import Main from "./pages/Main";
 
 // App function
 function App() {
     return (
-        <Router>
+        // <Router>
             <div>
-                <Route exact path="/" component={Main} />
+                <Main />
+                {/* <Route exact path="/" component={Main} /> */}
             </div>
-        </Router>
+        // </Router>
     );
 }
 
